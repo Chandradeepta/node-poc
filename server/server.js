c1cab460f1b42cfbf1b1eef7be27ef93a06d4c09
Chandradeepta <43542673+Chandradeepta@users.noreply.github.com>
2020-12-21 12:51:58 +0530

    removed dependencies

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config();
 const StudentService = require('./Services/StudentService');
 
 app.use(cors());
